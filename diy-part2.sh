014559071173e5bcb35b82e2d37c471cdee9ec61
OmyK <31337777+icons88@users.noreply.github.com>
2026-05-06 13:18:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,5 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# rm -rf feeds/luci/applications/luci-app-ksmbd
-# rm -rf feeds/packages/net/ksmbd-tools
+rm -rf feeds/luci/applications/luci-app-ksmbd
+rm -rf feeds/packages/net/ksmbd-tools
